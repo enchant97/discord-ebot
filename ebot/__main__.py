f8c369a770223f418eee868c23ff8295c9366c83
Leo Spratt <enchant97@users.noreply.github.com>
2021-04-02 21:39:44 +0100

    prepare for using database later

--- ebot/__main__.py
+++ ebot/__main__.py
@@ -1,9 +1,26 @@
+import asyncio
+
+from tortoise import Tortoise
+
 from .config import get_settings
-from .main import bot
+from .main import bot, init_db
 
 if __name__ == "__main__":
-    bot.run(
-        get_settings().TOKEN,
-        bot=True,
-        reconnect=True,
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(
+            asyncio.gather(
+                bot.start(
+                    get_settings().TOKEN,
+                    bot=True,
+                    reconnect=True,
+                ),
+                init_db(),
+                loop=loop,
+            )
         )
+    except KeyboardInterrupt:
+        loop.run_until_complete(bot.logout())
+        loop.run_until_complete(Tortoise.close_connections())
+    finally:
+        loop.close()
--- ebot/main.py
+++ ebot/main.py
@@ -1,7 +1,9 @@
 import discord
+from tortoise import Tortoise
 
 from .bot import Bot, get_prefix
 from .config import get_settings
+from .database import models
 
 cogs = [
     "ebot.cogs.owner",
@@ -20,3 +22,12 @@ async def on_ready():
     print(f"Logged in as: {bot.user.name} - {bot.user.id}")
     print(f"Discord Version: {discord.__version__}")
     print("bot now ready...")
+
+
+async def init_db():
+    await Tortoise.init(
+        db_url=get_settings().DB_URI,
+        modules={'models': [models],
+        }
+    )
+    await Tortoise.generate_schemas()
