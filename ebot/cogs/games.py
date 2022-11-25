a49b140fce8d0a2c0ad47633b5d1bc12a9064fee
Leo Spratt <enchant97@users.noreply.github.com>
2022-11-25 20:59:45 +0000

    add coin-flip & random number

---
+++ ebot/cogs/games.py
@@ -0,0 +1,26 @@
+import secrets
+
+import discord
+from discord.ext import commands
+from discord.ext.commands.context import Context
+
+
+class Games(commands.GroupCog):
+    def __init__(self, bot):
+        self.bot = bot
+        super().__init__()
+
+    @commands.hybrid_command(name="coin-flip", description="flip a coin")
+    async def coin_flip(self, ctx: commands.Context):
+        result = secrets.choice((":coin: Heads!", ":coin: Tail!"))
+        await ctx.reply(result)
+
+    @commands.hybrid_command(
+        name="random-number",
+        description="generate a random number from given range")
+    async def random_number(self, ctx: commands.Context, start: int, end: int):
+        result = secrets.choice(range(start, end+1))
+        await ctx.reply(f"Picked: {result}")
+
+async def init(bot):
+    await bot.add_cog(Games(bot))
